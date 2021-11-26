c3321b8c241b6d91ed3e9554bb04771c7c46a725
Cassandre <cassandre.michelet@gmail.com>
2021-11-26 15:34:20 +0100

    Discovering of the Container notions

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -5,20 +5,25 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Taxes\Calculator;
 
 class TestController
 {
+    /**
+     * @Route("/", name="index")
+     */
     public function index()
     {
         dd("Ca fonctionne");
     }
 
     /**
-     *  @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="localhost", schemes={"http, "https"})
+     *  @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, schemes={"http", "https"})
      */
-    public function test(Request $request, $age)
+    public function test(Request $request, $age, Calculator $calculator)
     {
-        // $age = $request->attributes->get('age');
+        $tva = $calculator->calcul(200);
+        dump($tva);
 
         return new Response("J'ai $age ans");
     }
